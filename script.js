a527f71c994f05063487712911d333e754eccfe9
khanyozondi <142215945+khanyozondi@users.noreply.github.com>
2023-12-18 07:10:37 +0200

    added search functionality using api

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function handleSearchSubmit (event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    
+function upadateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
 
 
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  console.log(response.data.temperature.current);
 }
 
+function searchCity(city) {
+  let apiKey = "o7bf242bb33dd170td84c97984f0fa4b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(upadateWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+  searchCity(searchInput.value);
+}
 
 let searchFormsElement = document.querySelector("#search-forms");
 searchFormsElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Durban");
